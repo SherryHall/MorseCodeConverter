1ee6b8a53e45da140788474665f6e73e150c69e4
SherryHall <slhall222@gmail.com>
2016-11-29 17:11:52 -0500

    Add class translate and load library

---
+++ MorseCodeConverter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeConverter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool keepGoing = true;
+			Translator currTranslate = new Translator();
+
+			
+			while(keepGoing)
+			{
+				Console.WriteLine("Do you want to translate a message to morse code (Y/N)?");
+				char answer = Console.ReadKey().KeyChar;
+				if (answer.Equals('Y') | answer.Equals('y'))
+				{
+
+				}
+			}
+			
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ MorseCodeConverter/Translator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeConverter
+{
+	class Translator
+	{
+		public Dictionary<char, string> AlphaToMorse = new Dictionary<char, string>();
+		public Dictionary<string, char> MorseToAlpha = new Dictionary<string, char>();
+		string fileName = "MorseCode.txt";
+
+		public Translator()
+		{
+			if (File.Exists(fileName))
+			{
+				// Load the Morse Code dictionaries from a csv file 
+				using (var codeFile = new StreamReader(fileName))
+				{
+
+					while (codeFile.Peek() > 0)        // while there is something in the file
+					{
+						var line = codeFile.ReadLine().Split(',');
+						var letter = Convert.ToChar(line[0]);
+						AlphaToMorse[letter] = line[1];
+						MorseToAlpha[line[1]] = letter;
+					}
+				}
+				//Console.WriteLine($"the code for c is{AlphaToMorse['C']}");
+				//Console.ReadLine();
+			}
+			else
+			{
+				Console.WriteLine("MorseCode.txt is missing so dictionary could not be built");
+				Console.ReadLine();
+			}
+
+		}
+	}
+}
