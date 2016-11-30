196943ded3b4da6d5f29b184451dbb370d02784d
SherryHall <slhall222@gmail.com>
2016-11-29 23:15:22 -0500

    Add code to decode morse code into message

--- MorseCodeConverter/Program.cs
+++ MorseCodeConverter/Program.cs
@@ -11,15 +11,18 @@ namespace MorseCodeConverter
 	{
 		static void Main(string[] args)
 		{
+			// Homework week 4 day 2.  Encode enter message into morse code. Decode morse code into message. Display past messages encoded.
+
 			bool keepGoing = true;
 			Translator currTranslate = new Translator();
 
-			
-			while(keepGoing)
+
+			while (keepGoing)
 			{
+				// Translate a message into Morse Code
 				Console.Write("Do you want to translate a message to morse code (Y/N)?  ");
-				char answer = Console.ReadKey().KeyChar;
-				if (answer.Equals('Y') | answer.Equals('y'))
+				char answer1 = Console.ReadKey().KeyChar;
+				if (answer1.Equals('Y') | answer1.Equals('y'))
 				{
 					Console.WriteLine("\nEnter the message to encode: (Alphanumberic characters only!)");
 					var message = Console.ReadLine();
@@ -29,8 +32,39 @@ namespace MorseCodeConverter
 				}
 				else
 				{
+					Console.WriteLine();
+					Console.WriteLine();
 					keepGoing = false;
 				}
+			}
+
+			keepGoing = true;
+			while (keepGoing)
+			{
+				// Translate Morse Code into a message 
+				Console.Write("Do you want to translate morse code into a message (Y/N)?  ");
+				char answer2 = Console.ReadKey().KeyChar;
+				if (answer2.Equals('Y') | answer2.Equals('y'))
+				{
+					Console.WriteLine("\nEnter the message to decode: (Dots and Dashes only! Separate letters with a space and words with /)");
+					var morseCode = Console.ReadLine();
+					var decodeString = currTranslate.DecodeIt(morseCode);
+					Console.WriteLine($"The message is:  {decodeString}");
+				}
+				else
+				{
+					Console.WriteLine();
+					keepGoing = false;
+				}
+			}
+
+			// Display past translations
+			Console.Write("Do you want to see the past messages encoded (Y/N?  ");
+			char answer3 = Console.ReadKey().KeyChar;
+			if (answer3.Equals('Y') | answer3.Equals('y'))
+			{
+				currTranslate.DisplayAllPrior();
+				Console.ReadLine();
 			}
 		}
 	}
--- MorseCodeConverter/Translator.cs
+++ MorseCodeConverter/Translator.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace MorseCodeConverter
 {
+
 	class Translator
 	{
 		public Dictionary<char, string> AlphaToCode = new Dictionary<char, string>();
 		public Dictionary<string, char> CodeToAlpha = new Dictionary<string, char>();
 		string keyFile = "MorseCode.csv";
+		string transFile = "Translations.csv";
 
 
 		public Translator()
@@ -33,7 +35,7 @@ namespace MorseCodeConverter
 			}
 			else
 			{
-				Console.WriteLine("MorseCode.txt is missing so dictionary could not be built");
+				Console.WriteLine("MorseCode.csv is missing so dictionary could not be built");
 				Console.ReadLine();
 			}
 		}
@@ -60,13 +62,47 @@ namespace MorseCodeConverter
 			return codeString;
 		}
 
+		public string DecodeIt(string morseCode)
+		{
+			// Translate morse code into message. letter is a string because morse code has multiple dashes and dots per letter 
+			string message = String.Empty;
+			char value;
+			var words = morseCode.Split('/');
+			foreach (string word in words)
+			{
+				var letters = word.Split( ' ');
+				foreach (string letter in letters)
+				{
+					if (CodeToAlpha.TryGetValue(letter, out value))
+					{
+						message += value;
+					}
+				}
+				// Add space to separate the words in the message
+				message +=  ' ';
+			}
+			return message;
+		}
 		public void SaveTranslation (string message, string codeString)
 		{
-			string transFile = "Translations.csv";
-			//using (var translations = new StreamWriter(transFile))
+			// Save the entered message and resulting morse code translation to a file
 			using (var translations = File.AppendText(transFile))
 			{
 				translations.WriteLine($"{message},{codeString}");
+			}
+		}
+
+		public void DisplayAllPrior()
+		{
+			using (var translations = new StreamReader(transFile))
+			{
+				// while there are lines remaining in the file, read the line, split into message and morse code, display to console
+				Console.WriteLine();
+				while (translations.Peek() > 0)
+				{
+					var line = translations.ReadLine().Split(',');
+					Console.WriteLine($"Message: {line[0]}      Morse Code: {line[1]}");
+				}
 			}
 		}
 	}
